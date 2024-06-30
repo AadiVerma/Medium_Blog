caebf4a8b8e8534907f5355afea87ada36e8d34f
git demo <adityakaplish11@gmail.com>
2024-06-30 14:07:01 +0530

    modefied

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import { Hono } from "hono";
 import { useRouter } from "./user";
 import { blogRouter } from "./blog";
+import {cors} from 'hono/cors';
 const app= new Hono();
+app.use('/api/*',cors());
 app.route('/api/v1/user',useRouter);
 app.route('api/v1/blog',blogRouter);
 
